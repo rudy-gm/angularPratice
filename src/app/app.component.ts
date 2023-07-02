e050e821267b3d87be3ed8f08cea0eec25fbab8b
Rudy <rodolfo-gm@hotmail.com>
2023-07-02 18:58:37 -0400

    third commit // creating components lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,67 +6,103 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Rudy';
-}
-
-let hello: string | number = "hello"; 
-
-hello = 123
-
-type Person = {
-  name: string;
-  age: number; 
-}
-
-
-//Funtions and Types 
-
-function add(a:number,b:number): number | string {
-  return a+b; 
 
 }
 
-function print(value:any){
-  console.log(value); 
-}
-
-//Generics
-
-function inserAtBeginning <T>(array: T[], value: T){
-  const newArray = [value, ...array];
-  return newArray; 
-}
-
-const demoArray = [1,2,3]; 
-
-const updateArray = inserAtBeginning(demoArray, -1); 
-const stringArray = inserAtBeginning(['a','b','c','d'], 'd'); 
 
 
-class Student {
-  // firstName: string;
-  // lastName: string;
-  // age:number; 
-  // courses: string[]; 
 
-  constructor(
-    public firstName: string,
-    public lastName: string,
-    public age:number,
-    public courses:string[]
-    )
-    {}
 
-  enroll(course:string){
-    this.courses.push(course); 
-  }
-}
 
-const student = new Student('Rudy', "Gutierrez", 21, ["Angular"]); 
-student.enroll("React"); 
 
-console.log(student); 
-console.log('testing git'); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+
+// let hello: string | number = "hello"; 
+
+// hello = 123
+
+// type Person = {
+//   name: string;
+//   age: number; 
+// }
+
+
+// //Funtions and Types 
+
+// function add(a:number,b:number): number | string {
+//   return a+b; 
+
+// }
+
+// function print(value:any){
+//   console.log(value); 
+// }
+
+// //Generics
+
+// function inserAtBeginning <T>(array: T[], value: T){
+//   const newArray = [value, ...array];
+//   return newArray; 
+// }
+
+// const demoArray = [1,2,3]; 
+
+// const updateArray = inserAtBeginning(demoArray, -1); 
+// const stringArray = inserAtBeginning(['a','b','c','d'], 'd'); 
+
+
+// class Student {
+//   // firstName: string;
+//   // lastName: string;
+//   // age:number; 
+//   // courses: string[]; 
+
+//   constructor(
+//     public firstName: string,
+//     public lastName: string,
+//     public age:number,
+//     public courses:string[]
+//     )
+//     {}
+
+//   enroll(course:string){
+//     this.courses.push(course); 
+//   }
+// }
+
+// const student = new Student('Rudy', "Gutierrez", 21, ["Angular"]); 
+// student.enroll("React"); 
+
+// console.log(student); 
+// console.log('testing git'); 
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServerComponentTest } from './server/server.componentTest';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServerComponentTest
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
