ba95265f2821411deae1c2f3ed581317c3a57330
Rudy <rodolfo-gm@hotmail.com>
2023-07-01 22:40:30 -0400

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name = 'Rudy';
+}
+
+let hello: string | number = "hello"; 
+
+hello = 123
+
+type Person = {
+  name: string;
+  age: number; 
+}
+
+
+//Funtions and Types 
+
+function add(a:number,b:number): number | string {
+  return a+b; 
+
+}
+
+function print(value:any){
+  console.log(value); 
+}
+
+//Generics
+
+function inserAtBeginning <T>(array: T[], value: T){
+  const newArray = [value, ...array];
+  return newArray; 
+}
+
+const demoArray = [1,2,3]; 
+
+const updateArray = inserAtBeginning(demoArray, -1); 
+const stringArray = inserAtBeginning(['a','b','c','d'], 'd'); 
+
+
+class Student {
+  // firstName: string;
+  // lastName: string;
+  // age:number; 
+  // courses: string[]; 
+
+  constructor(
+    public firstName: string,
+    public lastName: string,
+    public age:number,
+    public courses:string[]
+    )
+    {}
+
+  enroll(course:string){
+    this.courses.push(course); 
+  }
+}
+
+const student = new Student('Rudy', "Gutierrez", 21, ["Angular"]); 
+student.enroll("React"); 
+
+console.log(student); 
+
+
+
+
+
+
+
