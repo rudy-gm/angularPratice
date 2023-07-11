2f2b6e16b2187dea7147214e5606d10013ab1e55
rudy-gm <rodolfo-gm@hotmail.com>
2023-07-11 12:36:32 -0400

    15th // newDate and course task

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -12,6 +12,11 @@ export class ServersComponent {
   serverName =""; 
   userName = '';
   serverCreated = false; 
+  servers = ["server-1", 'server-2', 'server3']
+
+  // -- Assignment -- //; 
+  isShowing = true; 
+  numbers = []
 
   constructor(){
     setTimeout((()=>{
@@ -21,6 +26,8 @@ export class ServersComponent {
 
   onServerCreation(){
     this.serverCreated = !this.serverCreated; 
+    this.servers.push(this.serverName)
+    console.log(this.servers); 
   }
 
   onUpdateServerName(event: Event){
@@ -30,6 +37,18 @@ export class ServersComponent {
   deleteInput(){
     this.userName = ''
   }
+
+  showAndHide(){
+    this.isShowing = !this.isShowing; 
+    this.numbers.push(new Date())
+    console.log(this.numbers)
+  }
+
+  changeBackground(number){
+    return number >= 5 ? 'blue' : ''; 
+  }
+
+
 
 
 }
